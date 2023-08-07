46831c6bfdca6594b55dda34adc94a9d710d7611
Mehtab Alam <138979331+MehtabAlam2006@users.noreply.github.com>
2023-08-07 23:26:09 +0500

    Sizeof
    data types and it's size

---
+++ sizeof.cpp
@@ -0,0 +1,14 @@
+/*
+data type and size of it.
+*/
+#include<iostream>
+using namespace std;
+int main() {
+    // data types and their sizes.
+    
+cout<<"Size of integer : "<<sizeof(int)<<"Bytes \n";    
+cout<<"Size of character : "<<sizeof(char)<<"Bytes \n";
+cout<<"Size of String : " <<sizeof(string)<<"Bytes \n";
+cout<<"Size of boolean :"<<sizeof(bool)<<"Bytes \n";
+    return 0;
+}
