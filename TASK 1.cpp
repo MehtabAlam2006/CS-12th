7f3a841e2c4cb444c75c24c9283072e1637a3500
Mehtab Alam <138979331+MehtabAlam2006@users.noreply.github.com>
2023-08-07 22:46:39 +0500

    Task 1
    Declaration of variables
    giving a value to variables
    cout all variables

---
+++ TASK 1.cpp
@@ -0,0 +1,23 @@
+/* TASK 3
+Decleration of variables
+give a value to variables
+cout or prin all the variables
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+    string myText = "Mehtab Alam";
+    char Mycharacter = 'G';
+    int myNum = 14;
+    float myFloatNum = 5.5;
+    double myDoubleNum = 5.46464575;
+    bool myBoolean= "false";
+    
+    cout<< " My string : "<<myText <<endl;
+    cout<< " My character : "<<Mycharacter<<endl;
+    cout<< " My float Number : "<<myFloatNum<<endl;
+    cout<< " My double number : "<<myDoubleNum<<endl;
+    cout<< " My boolean :"<<myBoolean<<endl;
+    return 0;
+}
